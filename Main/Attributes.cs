c342ed15147fd1e3ee6e44ca662f7def5b206b93
Dima Enns <dieter.enns@gmail.com>
2023-10-15 12:08:03 +0200

    For MrMeeseeks.DIE version 3.0.0

---
+++ Main/Attributes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MrMeeseeks.DIE.UserUtility;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Parameter | AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
+public class InjectionKeyAttribute : Attribute
+{
+    public InjectionKeyAttribute(object key) => Key = key;
+
+    public object Key { get; }
+}
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public class DecorationOrdinalAttribute : Attribute
+{
+    public DecorationOrdinalAttribute(int ordinal) => Ordinal = ordinal;
+
+    public int Ordinal { get; }
+}
---
+++ Main/MarkerInterfaces.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+
+namespace MrMeeseeks.DIE.UserUtility;
+
+public interface IContainerInstance { }
+public interface ITransientScopeInstance { }
+public interface IScopeInstance { }
+
+public interface ITransientScopeRoot { }
+public interface IScopeRoot { }
+
+public interface ITransient { }
+public interface ISyncTransient { }
+public interface IAsyncTransient { }
+
+// ReSharper disable once UnusedTypeParameter
+public interface IDecorator<T> { }
+// ReSharper disable once UnusedTypeParameter
+public interface IComposite<T> { }
+
+public interface IInitializer
+{
+    void Initialize();
+}
+public interface ITaskInitializer
+{
+    Task InitializeAsync();
+}
+public interface IValueTaskInitializer
+{
+    ValueTask InitializeAsync();
+}
